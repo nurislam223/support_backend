5a2286d5900ebfba8056fcce57c153836d3a7f62
Nurislam <phlorristn@gmail.com>
2025-06-28 23:16:40 +0300

    fix jwt problem

--- support_backend/middleware.py
+++ support_backend/middleware.py
@@ -2,8 +2,7 @@ from fastapi import Request, Response
 from logger import log_request
 from typing import Callable
 
-async def log_requests_middleware(request: Request, call_next):
-    # Получаем пользователя из request.state.user
+async def log_requests_middleware(request: Request, call_next: Callable) -> Response:
     try:
         user = getattr(request.state, "user", {}).get("username", "-")
     except:
@@ -12,10 +11,8 @@ async def log_requests_middleware(request: Request, call_next):
     method = request.method
     endpoint = request.url.path
 
-    # Выполняем запрос
     response = await call_next(request)
 
-    # Логируем ответ
     log_request(
         user=user,
         method=method,
